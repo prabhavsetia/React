99b3ee1af5d855910c19a38156729ffc22ad2286
prabhavsetia <prabhav.setia21@gmail.com>
2024-06-30 15:34:45 +0530

    fetching data

--- wavenews/src/components/NewsItem.js
+++ wavenews/src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends PureComponent {
     return (
       <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://analyticsindiamag.com/wp-content/uploads/2024/06/Google-Gemini-Motorola.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a>
+            <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>
